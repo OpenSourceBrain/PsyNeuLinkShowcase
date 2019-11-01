ca1d5e07857f417fd4dee4248e2fb6c0f0d7cdca
Padraig Gleeson <p.gleeson@gmail.com>
2019-11-01 16:55:19 +0000

    Some test files

---
+++ PsyNeuLink/Simplest.py
@@ -0,0 +1,18 @@
+import psyneulink as pnl
+import psyneulink.core.components.functions.transferfunctions
+
+
+# Construct the Mechanisms:
+input_layer = pnl.ProcessingMechanism(size=5)
+hidden_layer = pnl.ProcessingMechanism(size=2, function=pnl.Logistic)
+output_layer = pnl.ProcessingMechanism(size=5, function=pnl.Logistic)
+
+# Construct the Composition:
+my_encoder = pnl.Composition()
+my_encoder.add_linear_processing_pathway([input_layer, hidden_layer, output_layer])
+
+print(my_encoder)
+
+my_encoder.show_graph(show_controller=True)
+
+print('Done!')
