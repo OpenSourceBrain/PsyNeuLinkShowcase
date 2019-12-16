a7465e41cfd672f4697432e7a49833cdd5754c0a
Katherine Mantel <k@mantel.pw>
2019-12-16 13:03:27 -0500

    Add basic PNL script with json

---
+++ PsyNeuLink/model_with_nested_graph.py
@@ -0,0 +1,24 @@
+import psyneulink as pnl
+
+comp = pnl.Composition(name='comp')
+inner_comp = pnl.Composition(name='Inner Composition')
+A = pnl.TransferMechanism(function=pnl.Linear(slope=5.0, intercept=2.0), name='A')
+B = pnl.TransferMechanism(function=pnl.Logistic, name='B')
+C = pnl.TransferMechanism(function=pnl.Exponential, name='C')
+
+E = pnl.TransferMechanism(name='E')
+F = pnl.TransferMechanism(name='F')
+
+
+for m in [E, F]:
+    inner_comp.add_node(m)
+
+
+for m in [A, B, C, inner_comp]:
+    comp.add_node(m)
+
+comp.add_projection(pnl.MappingProjection(), A, B)
+comp.add_projection(pnl.MappingProjection(), A, C)
+comp.add_projection(pnl.MappingProjection(), C, inner_comp)
+
+inner_comp.add_projection(pnl.MappingProjection(), E, F)
