ba2dd7adf6fd6a10458e80c1dd0fcf5254173070
Katherine Mantel <k@mantel.pw>
2019-12-16 19:13:07 -0500

    model_with_nested_graph.py: add run and value prints

--- PsyNeuLink/model_with_nested_graph.py
+++ PsyNeuLink/model_with_nested_graph.py
@@ -22,3 +22,9 @@ comp.add_projection(pnl.MappingProjection(), A, C)
 comp.add_projection(pnl.MappingProjection(), C, inner_comp)
 
 inner_comp.add_projection(pnl.MappingProjection(), E, F)
+
+comp.run(inputs={A: 1}, log=True)
+
+print(comp.results)
+for node in comp.nodes + inner_comp.nodes:
+    print(f'{node.name}: {node.parameters.value.get(comp)}')
