da2b2f82504e8fafaceee057b2cc015d5bb77dd7
Padraig Gleeson <p.gleeson@gmail.com>
2020-02-03 14:48:31 +0000

    Regenerated

--- NeuroML2/regenerateAndTest.sh
+++ NeuroML2/regenerateAndTest.sh
@@ -3,11 +3,14 @@ set -e
 python ABC.py -jnml
 python ABC.py -pnl
 jnml LEMS_SimABC.xml -graph
+jnml LEMS_SimABC.xml -nogui
 
 
 python McCP.py -jnml
 python McCP.py -pnl
 jnml LEMS_SimMcCPNet.xml -graph
+jnml LEMS_SimMcCPNet.xml -nogui
+
 
 jnml LEMS_FitzHughNagumo.xml -nogui
 jnml LEMS_FitzHughNagumo.xml -graph
